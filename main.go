cbc49215d7847951748cabe29f3d2a26ff7039e5
Harrison <harrison@theshoebridges.com>
2015-09-04 21:38:23 -0400

    Add login handler

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/paked/gerrycode/communicator"
+	"github.com/paked/restrict"
 )
 
 var (
@@ -18,6 +21,14 @@ type User struct {
 	ID       int64
 	Username string
 	Password string
+}
+
+func (u *User) Login(password string) bool {
+	if password == u.Password { // This should be moved to bcrypt at some point
+		return true
+	}
+
+	return false
 }
 
 func main() {
@@ -31,10 +42,17 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/user/register", RegisterUserHandler).Methods("POST")
+	r.HandleFunc("/user/login", LoginUserHandler).Methods("POST")
+
+	r.HandleFunc("/secret", restrict.R(SecretHandler)).Methods("GET")
 
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func SecretHandler(w http.ResponseWriter, r *http.Request, t *jwt.Token) {
+	communicator.New(w).OK("This page is secret!")
 }
 
 func RegisterUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -77,10 +95,42 @@ func RegisterUserHandler(w http.ResponseWriter, r *http.Request) {
 	c.OKWithData("Here is your user", u)
 }
 
+func LoginUserHandler(w http.ResponseWriter, r *http.Request) {
+	c := communicator.New(w)
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	u, err := GetUser("username", username)
+	if err != nil {
+		log.Println(err)
+		c.Fail("Could not get user")
+		return
+	}
+
+	ok := u.Login(password)
+	if !ok {
+		c.Fail("Incorrect username")
+		return
+	}
+
+	claims := make(map[string]interface{})
+	claims["id"] = u.ID
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	ts, err := restrict.Token(claims)
+	if err != nil {
+		c.Fail("Failure signing that token!")
+		return
+	}
+
+	c.OKWithData("Here is your token", ts)
+}
+
 func GetUser(key, value string) (User, error) {
 	u := User{}
 
-	row := db.QueryRow("SELECT id, username, password, FROM users WHERE "+key+" = ?", value)
+	row := db.QueryRow("SELECT id, username, password FROM users WHERE "+key+" = ?", value)
 	err := row.Scan(&u.ID, &u.Username, &u.Password)
 	if err != nil {
 		return u, err
